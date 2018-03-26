1c4e77774fd55854b7ad2311b4e36bb6b590ab8a
Mobileapps <anbarasuv@ihorsetechnologiesc.com>
2018-03-26 15:53:16 +0530

    initial commit

---
+++ webapp.jenkins/src/test/java/automation/webapp/jenkins/AppTest.java
@@ -0,0 +1,29 @@
+package automation.webapp.jenkins;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+	public void testApp()
+    {
+    	System.out.println("helllo log");
+        AssertJUnit.assertTrue( true );
+    }
+}
