469afe0378543fcd69c7f7d860eacdff8422a428
Mobileapps <anbarasuv@ihorsetechnologiesc.com>
2018-04-19 10:52:43 +0530

    Web automation in HeadLess Browser

--- src/test/java/automation/webapp/jenkins/AppTest.java
+++ src/test/java/automation/webapp/jenkins/AppTest.java
@@ -3,8 +3,12 @@ package automation.webapp.jenkins;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.testng.Assert;
 import org.testng.AssertJUnit;
 
@@ -13,18 +17,21 @@ import org.testng.AssertJUnit;
  */
 public class AppTest
 {
-	private WebDriver driver;		
+	HtmlUnitDriver driver;
 
     @Test
 	public void testApp()
     {
     	driver.get("http://demo.guru99.com/test/guru99home/");  
 		String title = driver.getTitle();				 
-		Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+		Assert.assertTrue(title.contains("Demo Guru99 Page")); 	
+		String domainName = (String) driver.executeScript("return document.domain");
     }
     @BeforeTest
 	public void beforeTest() {	
-	    driver = new FirefoxDriver();  
+    
+    	driver = new HtmlUnitDriver(BrowserVersion.INTERNET_EXPLORER_11);
+    	driver.setJavascriptEnabled(true);
 	}		
 	@AfterTest
 	public void afterTest() {
