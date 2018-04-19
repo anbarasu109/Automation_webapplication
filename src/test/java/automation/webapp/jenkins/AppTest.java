78ae188aec4afb599d57dbde525aa17cb0b4dcba
Mobileapps <anbarasuv@ihorsetechnologiesc.com>
2018-04-19 11:52:36 +0530

    Firefox Headless Browser

--- src/test/java/automation/webapp/jenkins/AppTest.java
+++ src/test/java/automation/webapp/jenkins/AppTest.java
@@ -7,7 +7,9 @@ import org.testng.annotations.Test;
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.testng.Assert;
 import org.testng.AssertJUnit;
@@ -17,24 +19,32 @@ import org.testng.AssertJUnit;
  */
 public class AppTest
 {
-	HtmlUnitDriver driver;
+	FirefoxBinary firefoxBinary;
+	FirefoxOptions firefoxOptions;
+	FirefoxDriver driver;
 
     @Test
 	public void testApp()
     {
+    	try {
     	driver.get("http://demo.guru99.com/test/guru99home/");  
 		String title = driver.getTitle();				 
 		Assert.assertTrue(title.contains("Demo Guru99 Page")); 	
+		System.out.print(title);
 		String domainName = (String) driver.executeScript("return document.domain");
+		System.out.println(domainName);
+    	} finally {
+    		driver.quit();
+    	}
     }
     @BeforeTest
 	public void beforeTest() {	
-    
-    	driver = new HtmlUnitDriver(BrowserVersion.INTERNET_EXPLORER_11);
-    	driver.setJavascriptEnabled(true);
-	}		
-	@AfterTest
-	public void afterTest() {
-		driver.quit();			
-	}	
+    	firefoxBinary = new FirefoxBinary();
+    	firefoxBinary.addCommandLineOptions("--headless");
+    	System.setProperty("webdriver.gecko.driver", "/usr/local/Cellar/geckodriver/0.20.1/bin/geckodriver");
+    	firefoxOptions = new FirefoxOptions();
+    	firefoxOptions.setBinary(firefoxBinary);
+    	driver = new FirefoxDriver(firefoxOptions);
+	}			
 }
+
